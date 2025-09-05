47caac06da7147d32a6f2c3237d019b157f2b8bc
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-05 16:53:25 +0200

    kk

--- 2tip/2tip_web/cw1/Program.cs
+++ 2tip/2tip_web/cw1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("to dziala");
+Console.Write("powiedz mi cos: ");
+string? userInput = Console.ReadLine();
+Console.WriteLine("powiedziałeś: " + userInput);
