2d50538b0f8b5c6208a513819be2907b83cafda0
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-08 11:34:58 +0200

    kk

--- 2tip/2tip_web/cw1/Program.cs
+++ 2tip/2tip_web/cw1/Program.cs
@@ -3,4 +3,15 @@
 // Console.WriteLine("to dziala");
 Console.Write("powiedz mi cos: ");
 string? userInput = Console.ReadLine();
+//Console.Read();
 Console.WriteLine("powiedziałeś: " + userInput);
+Console.Write("Podaj liczbę: ");
+try {
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Twoja liczba to: " + number);
+}
+catch (Exception e) {
+  Console.WriteLine(e.Message);
+  Console.WriteLine("Wystąpił błąd podczas konwersji.");
+}
+Console.WriteLine("Koniec programu.");
---
+++ 2tip/2tip_web/zad1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 12;
+int b = 45;
+
+Console.WriteLine(a + " + " + b + " = " + (a + b));
