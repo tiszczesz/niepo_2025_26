ea4a94b5b16ac42137fe1065121631819129d5fd
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-08 09:11:41 +0200

    kkk

---
+++ 4tip/4tip_web_server/cw1_ef_sqlite/cw1_ef_sqlite/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cw1_ef_sqlite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 4tip/4tip_web_server/cw1_ef_sqlite/cw1_ef_sqlite/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace cw1_ef_sqlite.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Tytuł")]
+        public string Title { get; set; }
+
+        [DisplayName("Autor")]
+        public string Author { get; set; }
+
+        [DisplayName("Rok wydania")]
+        public int Year { get; set; }
+    }
+}
