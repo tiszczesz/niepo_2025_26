5973f085d94fac875fedaa4cc7856248faace97c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-08 09:23:03 +0200

    kk

--- 4tip/4tip_web_server/cw1_ef_sqlite/cw1_ef_sqlite/Program.cs
+++ 4tip/4tip_web_server/cw1_ef_sqlite/cw1_ef_sqlite/Program.cs
@@ -1,5 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using cw1_ef_sqlite.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//pobieramy connection string z pliku appsettings.json
+var connString = builder.Configuration.GetConnectionString("sqlite");
+builder.Services
+    .AddDbContext<BookContext>(
+        options => options.UseSqlite(connString)
+        );
 var app = builder.Build();
 app.UseStaticFiles();
 //app.MapGet("/", () => "Hello World!");
