2ce29b35e8041866aeca5fe59a5d14699d610903
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-11 11:35:49 +0200

    kk

--- 3tip/3tip_desk/SummerEx/Form1.cs
+++ 3tip/3tip_desk/SummerEx/Form1.cs
@@ -1,10 +1,18 @@
+﻿using SummerEx.Models;
+
 namespace SummerEx
 {
-    public partial class Form1 : Form
-    {
+    public partial class Form1 : Form {
+        private readonly HolidaysRepo _repo;
         public Form1()
         {
             InitializeComponent();
+            _repo = new HolidaysRepo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
---
+++ 3tip/3tip_desk/SummerEx/Models/HolidaysRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace SummerEx.Models
+{
+    public class HolidaysRepo
+    {
+        private string connString = "Data Source=DAL/summerdb.db";
+
+        public List<Holiday> GetHolidays() {
+            List<Holiday> holidays = new List<Holiday>();
+            using SqliteConnection conn = new SqliteConnection(connString);
+            using SqliteCommand command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM Holidays";
+            conn.Open();
+            using SqliteDataReader reader = command.ExecuteReader();
+
+            return holidays;
+        }
+    }
+}
