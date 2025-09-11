a8dee9f461e1e0d9d57aff50c6d222645498e946
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-11 11:21:37 +0200

    kk

---
+++ 3tip/3tip_desk/SummerEx/Models/Holiday.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummerEx.Models
+{
+    public class Holiday
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Miejsce")]
+        public string? Place { get; set; }
+
+        [DisplayName("Data początkowa")]
+        public DateTime DateStart { get; set; }
+
+        [DisplayName("Data początkowa")]
+        public DateTime DateEnd { get; set; }
+
+        [DisplayName("Cena")]
+        public decimal Price { get; set; }
+        
+        [DisplayName("Ilość osób")]
+        public int Count { get; set; }
+        
+        [DisplayName("Wyżywienie w cenie")]
+        public bool IsFoodIncluded { get; set; }
+
+    }
+}
