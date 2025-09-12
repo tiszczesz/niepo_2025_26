d53d1c2f8ba31820399d87a78479c89c8d33a429
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-12 14:19:52 +0200

    kk

---
+++ 3tip/3tip_web/cw1_msunit_test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using cw1.Models;
+
+namespace cw1_msunit_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void CanAgeGoatNegative() {
+            var goat = new Goat();
+            goat.Age = -1;
+            Assert.IsFalse(goat.Age<0);
+        }
+    }
+}
