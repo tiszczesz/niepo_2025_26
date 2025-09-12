1a3f393f910d36206a3470b4ba6c8aba82b9cd30
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-12 11:03:42 +0200

    kk

--- 4tip/4tip_desk/cw2_EF_Sqlite/cw2_EF_Sqlite/Form1.cs
+++ 4tip/4tip_desk/cw2_EF_Sqlite/cw2_EF_Sqlite/Form1.cs
@@ -4,6 +4,8 @@ namespace cw2_EF_Sqlite
 {
     public partial class Form1 : Form {
         private StudentDbContext _context;
+        //public List<Student> students;
+        public BindingSource bindingSource = new BindingSource();
         public Form1()
         {
             InitializeComponent();
@@ -11,6 +13,10 @@ namespace cw2_EF_Sqlite
 
         private void Form1_Load(object sender, EventArgs e) {
             _context = new StudentDbContext();
+            //students = _context.Students.ToList();
+            bindingSource.DataSource = _context.Students.ToList();
+            
+            dataGridView1.DataSource = bindingSource;
         }
     }
 }
