30f444e64bf75601f8aae8a1e75cf4f67df75be2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-18 15:38:42 +0200

    kk

--- 5tip/5tip_web/ark1/ark1/src/data/info.ts
+++ 5tip/5tip_web/ark1/ark1/src/data/info.ts
@@ -1,3 +1,7 @@
 export const Greeting = ["Dzień dobry", "Good morning",
-    "Buenos dias", "Hej", "Siema"];
-export const textDecorationOptions = ["none", "underline", "overline", "line-through", "underline overline"];
+    "Buenos dias", "Hej", "Siema","Cześć", "Servus", "Hallo", "Salut", "Buongiorno"];
+export const textDecorationOptions = ["none",
+    "underline",
+    "overline",
+    "line-through",
+    "underline overline"];
