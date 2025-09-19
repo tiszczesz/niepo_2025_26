0e84e8c03eddd373cd31fde180e659b9ee00c84a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-19 10:11:06 +0200

    iteracyjnie

--- 4tip/4tip_desk/Nwd.Solution/Nwd.ClassLib/NwdCalculator.cs
+++ 4tip/4tip_desk/Nwd.Solution/Nwd.ClassLib/NwdCalculator.cs
@@ -9,6 +9,7 @@ namespace Nwd.ClassLib
     public class NwdCalculator
 
     {
+        //rekurencyjnie
         public int CalculateNwd(int a, int b) {
             if (a==0 && b==0) 
                   throw new ArgumentException(
@@ -18,6 +19,21 @@ namespace Nwd.ClassLib
             b = Math.Abs(b);
             if (b==0) return a;
             return CalculateNwd(b, a % b);
+        }
+        //iteracyjnie
+        public int CalculateNwdIteratively(int a, int b) {
+            if (a==0 && b==0) 
+                  throw new ArgumentException(
+                      "NWD is not defined for both numbers being zero."
+                      );
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
         }
     }
 }
--- 4tip/4tip_desk/Nwd.Solution/Nwd.UnitTests/NwdTunitTsts.cs
+++ 4tip/4tip_desk/Nwd.Solution/Nwd.UnitTests/NwdTunitTsts.cs
@@ -1,12 +1,9 @@
 ﻿using Nwd.ClassLib;
 
-namespace Nwd.UnitTests
-{
-    public class NwdTunitTsts
-    {
+namespace Nwd.UnitTests {
+    public class NwdTunitTsts {
         [Fact]
-        public void Nwd_Of_48_18_Is_6()
-        {
+        public void Nwd_Of_48_18_Is_6() {
             int a = 48;
             int b = 18;
             int expectedNwd = 6;
@@ -14,9 +11,9 @@ namespace Nwd.UnitTests
             int actualNwd = calculator.CalculateNwd(a, b);
             Assert.Equal(expectedNwd, actualNwd);
         }
+
         [Fact]
-        public void Nwd_Of_101_10_Is_1()
-        {
+        public void Nwd_Of_101_10_Is_1() {
             int a = 101;
             int b = 10;
             int expectedNwd = 1;
@@ -24,9 +21,9 @@ namespace Nwd.UnitTests
             int actualNwd = calculator.CalculateNwd(a, b);
             Assert.Equal(expectedNwd, actualNwd);
         }
+
         [Fact]
-        public void Nwd_0f_0_5_Is_5()
-        {
+        public void Nwd_0f_0_5_Is_5() {
             int a = 0;
             int b = 5;
             int expectedNwd = 5;
@@ -34,9 +31,9 @@ namespace Nwd.UnitTests
             int actualNwd = calculator.CalculateNwd(a, b);
             Assert.Equal(expectedNwd, actualNwd);
         }
+
         [Fact]
-        public void Nwd_0f_5_0_Is_5()
-        {
+        public void Nwd_0f_5_0_Is_5() {
             int a = 5;
             int b = 0;
             int expectedNwd = 5;
@@ -44,9 +41,9 @@ namespace Nwd.UnitTests
             int actualNwd = calculator.CalculateNwd(a, b);
             Assert.Equal(expectedNwd, actualNwd);
         }
+
         [Fact]
-        public void Nwd_0f_negative5_7_Is_1()
-        {
+        public void Nwd_0f_negative5_7_Is_1() {
             int a = -5;
             int b = 7;
             int expectedNwd = 1;
@@ -54,14 +51,34 @@ namespace Nwd.UnitTests
             int actualNwd = calculator.CalculateNwd(a, b);
             Assert.Equal(expectedNwd, actualNwd);
         }
+
         [Fact]
-        public void Nwd_0f_0_0_Is_Exception()
-        {
+        public void Nwd_0f_0_0_Is_Exception() {
             int a = 0;
             int b = 0;
             int expectedNwd = 1;
             NwdCalculator calculator = new NwdCalculator();
             Assert.Throws<ArgumentException>(() => calculator.CalculateNwd(a, b));
+        }
+
+        [Fact]
+        public void Nwd_Iter_Of_270_192_Is_6() {
+            int a = 270;
+            int b = 192;
+            int expectedNwd = 6;
+            NwdCalculator calculator = new NwdCalculator();
+            int actualNwd = calculator.CalculateNwdIteratively(a, b);
+            Assert.Equal(expectedNwd, actualNwd);
+        }
+        [Fact]
+        public void Nwd_Iter_Of_18_0_Is_18()
+        {
+            int a = 18;
+            int b = 0;
+            int expectedNwd = 18;
+            NwdCalculator calculator = new NwdCalculator();
+            int actualNwd = calculator.CalculateNwdIteratively(a, b);
+            Assert.Equal(expectedNwd, actualNwd);
         }
 
     }
