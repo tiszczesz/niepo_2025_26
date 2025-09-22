48409f01366e79747003466ae6d795cbc5b33a3b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-22 11:35:36 +0200

    kk

---
+++ 2tip/2tip_desk/cw3/Program.cs
@@ -0,0 +1,29 @@
+﻿//ex1();
+ext2();
+void ex1()
+{
+    string[] lines = new string[]{
+        "First line",
+        "Second line",
+        "Third line"
+    };
+    File.WriteAllLines("output.txt", lines);
+}
+void ext2()
+{
+    const string filePath = "output.txt";
+    if (File.Exists(filePath))
+    {
+        //wczytanie wszystkich linii z pliku
+        string[] result = File.ReadAllLines(filePath);
+        //wypisanie wczytanych linii na konsolę
+        foreach (var line in result)
+        {
+            Console.WriteLine($"{line} ma {line.Length} znaków");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Plik {filePath} nie istnieje.");
+    }
+}
