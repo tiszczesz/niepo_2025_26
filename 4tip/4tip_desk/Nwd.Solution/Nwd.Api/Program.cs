22d2e84c1261d5f5e08f686bfb5fc1f2b62c0caa
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-09-22 09:53:38 +0200

    kk

--- 4tip/4tip_desk/Nwd.Solution/Nwd.Api/Program.cs
+++ 4tip/4tip_desk/Nwd.Solution/Nwd.Api/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Nwd.ClassLib;
 
+NwdCalculator calculator = new NwdCalculator();
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container. CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
+var app = builder.Build();
+// Configure the HTTP request pipeline. CORS
+app.UseCors();
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/nwd/{a}/{b}", (int a, int b) => calculator.CalculateNwd(a, b));
 
 app.Run();
