dc2384ef48a795df2f7518830f0e39ea97c8c271
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-13 09:29:36 +0200

    kk

---
+++ 4tip/4tip_type_script/cw2/dist/ex1.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function fun1() {
+    const a = 5;
+    console.log(`typ zmiennej a to ${typeof a} a = ${a}`);
+}
+function Repeater(a, text) {
+    for (let i = 0; i < a; i++) {
+        console.log(text);
+    }
+}
+fun1();
+//# sourceMappingURL=ex1.js.map
