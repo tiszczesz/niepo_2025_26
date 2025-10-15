a800f906643037d15b1e3601e3732e539766116a
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-15 08:45:24 +0200

    kk

--- 2tip/2tip_desk/cw4_class/Nwd.cs
+++ 2tip/2tip_desk/cw4_class/Nwd.cs
@@ -21,4 +21,22 @@ public class Nwd
     {
         return $"a: {A}, b: {B}";
     }
+    //rekurencyjnie obliczanie NWD
+    public int NwdRek(int a, int b)
+    {
+        if (b == 0) return a;
+        return NwdRek(b, a % b);
+    }
+    //iteracyjnie obliczanie NWD
+    public int NwdIter(int a, int b)
+    {
+        int temp = b;
+        while (b != 0)
+        {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
--- 2tip/2tip_desk/cw4_class/Program.cs
+++ 2tip/2tip_desk/cw4_class/Program.cs
@@ -3,4 +3,18 @@
 Nwd nwd = new Nwd();
 nwd.A = -10;
 nwd.B = 15;
-Console.WriteLine(nwd);
+Console.WriteLine($"NwdRek({nwd.A}, {nwd.B}) = {nwd.NwdRek(nwd.A, nwd.B)}");
+Console.WriteLine($"NwdIter({nwd.A}, {nwd.B}) = {nwd.NwdIter(nwd.A, nwd.B)}");
+//Console.WriteLine(nwd);
+
+//przyklad rekurencji
+// void Repeat(int count)
+// {
+//     //wchodzenie w głąb stosu wywołań
+//     Console.WriteLine("Hello " + count);
+//     //warunek rekurencji
+//     if (count > 0) Repeat(count - 1);
+//     //wychodzenie ze stosu wywołań
+//     Console.WriteLine("Hello " + count);
+// }
+// Repeat(5);
