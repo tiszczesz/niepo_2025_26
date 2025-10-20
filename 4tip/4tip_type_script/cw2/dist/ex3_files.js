c319acf61156208188dcefba29f20683915128a0
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-20 08:41:55 +0200

    kk

--- 4tip/4tip_type_script/cw2/dist/ex3_files.js
+++ 4tip/4tip_type_script/cw2/dist/ex3_files.js
@@ -1,8 +1,13 @@
-import { readFileSync } from "node:fs";
+/// <reference types="node" />
+import { readFileSync, writeFileSync } from "node:fs";
 import { countWords } from "./ex3_files_module.js";
+import { students } from "./ex3_data.js";
 const data = readFileSync('./input.txt', 'utf-8');
 const words = data.split(/\s+/);
 //const words: string[] = data.split(' ');
 console.log(words);
 console.log(`Number of words with 3 letters: ${countWords(3, words)}`);
+students.forEach((student) => {
+    console.log(`${student.firstname} ${student.lastname}, age: ${student.age}`);
+});
 //# sourceMappingURL=ex3_files.js.map
--- 4tip/4tip_type_script/cw2/src/ex3_files.ts
+++ 4tip/4tip_type_script/cw2/src/ex3_files.ts
@@ -1,7 +1,13 @@
-import { readFileSync,writeFileSync } from "node:fs";
+/// <reference types="node" />
+import { readFileSync, writeFileSync } from "node:fs";
 import { countWords } from "./ex3_files_module.js";
-const data:string = readFileSync('./input.txt', 'utf-8');
+import { type Student, students } from "./ex3_data.js";
+const data: string = readFileSync('./input.txt', 'utf-8');
 const words: string[] = data.split(/\s+/);
 //const words: string[] = data.split(' ');
 console.log(words);
 console.log(`Number of words with 3 letters: ${countWords(3, words)}`);
+students.forEach(
+    (student) => {
+        console.log(`${student.firstname} ${student.lastname}, age: ${student.age}`);
+    });
