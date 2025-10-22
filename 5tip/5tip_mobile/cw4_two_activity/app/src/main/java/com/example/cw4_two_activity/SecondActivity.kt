a9424dd6ce70196699e3f059713c75878033dc88
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-22 15:05:09 +0200

    kkk

---
+++ 5tip/5tip_mobile/cw4_two_activity/app/src/main/java/com/example/cw4_two_activity/SecondActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cw4_two_activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn = findViewById<android.widget.Button>(R.id.button2);
+        btn.setOnClickListener {
+            finish()
+        }
+    }
+}
