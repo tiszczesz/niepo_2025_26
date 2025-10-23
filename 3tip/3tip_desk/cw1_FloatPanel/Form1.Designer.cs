7c7ba6dc275a34afd9a082acd1dc224d0e8261e2
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-23 11:24:02 +0200

    kkk

---
+++ 3tip/3tip_desk/cw1_FloatPanel/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace cw1_FloatPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            numUpDown = new NumericUpDown();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numUpDown).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 51);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(800, 399);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Info;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(numUpDown);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 51);
+            panel1.TabIndex = 1;
+            // 
+            // numUpDown
+            // 
+            numUpDown.Location = new Point(12, 12);
+            numUpDown.Maximum = new decimal(new int[] { 50, 0, 0, 0 });
+            numUpDown.Minimum = new decimal(new int[] { 10, 0, 0, 0 });
+            numUpDown.Name = "numUpDown";
+            numUpDown.Size = new Size(120, 23);
+            numUpDown.TabIndex = 0;
+            numUpDown.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // button1
+            // 
+            button1.Location = new Point(164, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(321, 33);
+            button1.TabIndex = 1;
+            button1.Text = "Generuj";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)numUpDown).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Button button1;
+        private NumericUpDown numUpDown;
+    }
+}
---
+++ 3tip/3tip_desk/cw1_FloatPanel/Form1.cs
@@ -0,0 +1,36 @@
+﻿namespace cw1_FloatPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            flowLayoutPanel1.Dock = DockStyle.Fill;              // wypełnij rodzica
+            flowLayoutPanel1.FlowDirection = FlowDirection.LeftToRight; // jak flex-direction: row
+            flowLayoutPanel1.WrapContents = true;                // zawijanie linii
+            flowLayoutPanel1.AutoScroll = true;                  // pasek przewijania przy potrzebie
+            flowLayoutPanel1.Padding = new Padding(10);
+            flowLayoutPanel1.AutoSize = false;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            // Dodaj kontrolki dynamicznie
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int limit = Convert.ToInt32(numUpDown.Value);
+            for (int i = 0; i < limit; i++)
+            {
+                Panel p = new Panel();
+                Label label = new Label();
+                label.Text = $"Panel {i + 1}";
+                p.Controls.Add(label);
+                p.Margin = new Padding(5);
+                p.Height = 100;
+                p.Width = 100;
+                p.BackColor = Color.Orange;
+                flowLayoutPanel1.Controls.Add(p);
+            }
+        }
+    }
+}
