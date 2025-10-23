aaa7c8f48755c9477e769d6d9502f684db6f604c
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-23 12:01:06 +0200

    kk

--- 3ti_inf/cw5/cw5.js
+++ 3ti_inf/cw5/cw5.js
@@ -1,3 +1,29 @@
+
+
+//inny sposob definicji funkcji działa jak zmienna
+//mozna jej uzyc dopiero po zdefiniowaniu!!!!!
+const showDate = function () {
+    console.log(new Date());
+};
+
+showDate(); //wywołanie funkcji ale dopiero po zdefiniowaniu
+
+//definicja funkcji strzałkowej
+const showTime = (text) => {
+    console.log(text + " " + new Date().toLocaleTimeString());
+};
+showTime("aktualna godzina to: "); //wywołanie funkcji strzałkowej
+//wywołanie funkcji
+//szukamy elementu o id result1
+const result1Element = document.querySelector("#result1");
+const result2Element = document.querySelector("#result2");
+if (result1Element !== null) {
+    InsertText(result1Element, "lightblue", "ala ma kota"); //użycie funkcji
+}
+if (result2Element !== null) {
+    InsertText(result2Element, "green", "to jest inny tekst"); //użycie funkcji
+}
+
 //definicja funkcji
 function InsertText(elem, color, text) {
     //ciało funkcji
@@ -9,14 +35,4 @@ function GenerSelect() {
     for (let i = 1; i <= 10; i++) {
 
     }
-}
-//wywołanie funkcji
-//szukamy elementu o id result1
-const result1Element = document.querySelector("#result1");
-const result2Element = document.querySelector("#result2");
-if (result1Element !== null) {
-    InsertText(result1Element, "lightblue", "ala ma kota"); //użycie funkcji
-}
-if (result2Element !== null) {
-    InsertText(result2Element, "green", "to jest inny tekst"); //użycie funkcji
 }
