abdbf8d951515a9e402730bd9c9f2fc5dc9a52eb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-27 10:34:07 +0100

    kk

--- 3ti_inf/cw7/cw7.js
+++ 3ti_inf/cw7/cw7.js
@@ -1,2 +1,18 @@
+//wyszukanie elementów przycisku i pola tekstowego
 const btnCreate = document.querySelector("#btnCreate");
-btnCreate.onclick = function(){};
+const input = document.querySelector("#inputText");
+const result = document.querySelector("#result");
+btnCreate.onclick = function () {
+    //pobranie wartości z pola tekstowego 
+    // i konwersja na liczbę całkowitą lub NaN
+    const size = parseInt(input.value);
+    //sprawdzamy czy wartość jest liczbą całkowitą
+    if (isNaN(size)) return;
+    //tworzymy tablicę i wypelniamy ją losowymi liczbami
+    const numbers = []; //pusta tablica
+    for (let i = 0; i < size; i++) {
+        numbers.push(Math.floor(Math.random() * 100));//[0-99]
+    }
+    console.log(numbers);
+    result.innerHTML = "Wylosowano liczby: " + numbers.join(" ");
+};
