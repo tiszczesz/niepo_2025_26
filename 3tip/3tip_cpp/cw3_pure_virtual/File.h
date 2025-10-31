43ca3ecec8ee74ac0741401666ce1d0d5abce287
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-10-31 09:00:27 +0100

    kk

---
+++ 3tip/3tip_cpp/cw3_pure_virtual/File.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <iostream>
+
+class File
+{
+protected:
+	std::string path;
+	int size;
+public:
+	File() : path("noname"), size(0) {
+		std::cout
+		<< "Utworzenie obiektu File (domyslny konstruktor)"
+		<< std::endl;
+	}
+	File(const std::string& path,int size)
+		:path(path), size(size) {
+		std::cout
+			<< "Utworzenie obiektu File (z argumentami konstruktor)"
+			<< std::endl;
+	}
+	virtual ~File() {
+		std::cout
+			<< "Usuniecie obiektu File"
+			<< std::endl;
+	}
+};
+
---
+++ 3tip/3tip_cpp/cw3_pure_virtual/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include "File.h"
+using namespace std;
+void Pointers() {
+	if (true) {
+		File f1;
+		File f2("file.txt", 1024);
+		File* pf1 = new File("plik i wskaznik", 200);
+		cout << pf1 << endl;
+		delete pf1; // usuniecie obiektu wskazywanego przez pf1
+		cout << pf1 << endl;
+		pf1 = nullptr; //zeby nie wskazywal na usuniety obiekt
+		// ... dalsza czesc programu
+		cout << pf1 << endl;
+	}
+	//delete pf1; !!!!!!!!!!!!!!!!!!!!
+	//pf1->ggg
+	cout << "Koniec funkcji" << endl;
+}
+int main() {
+	
+	return 0;
+}
