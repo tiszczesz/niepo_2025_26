a9b53ea28fbee509e657f92110725e3c23a2fa1d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-03 11:32:43 +0100

    kk

---
+++ 2tip/2tip_desk/cw6_wf/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace cw6_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(227, 126);
+            label1.Name = "label1";
+            label1.Size = new Size(310, 65);
+            label1.TabIndex = 0;
+            label1.Text = "To jest Form1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(23, 365);
+            button1.Name = "button1";
+            button1.Size = new Size(242, 52);
+            button1.TabIndex = 1;
+            button1.Text = "Nowe okienko";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(530, 365);
+            button2.Name = "button2";
+            button2.Size = new Size(242, 52);
+            button2.TabIndex = 2;
+            button2.Text = "Zamknij aplikację";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ 2tip/2tip_desk/cw6_wf/Form1.cs
@@ -0,0 +1,26 @@
+﻿namespace cw6_wf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Tworzymy obiekt okna Form2
+            Form2 form2 = new Form2(this);
+            //Wyświetlamy okno Form2 jako okno modalne
+            form2.ShowDialog();
+            //Wyświetlamy okno Form2 jako okno niemodalne
+           // form2.Show();
+           //this.Visible = false;
+        }
+    }
+}
