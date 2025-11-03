829fa99432e68d3e2718f52efd08d04ed187708b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-03 10:02:41 +0100

    kk

--- 3ti_inf/cw7/cw7.js
+++ 3ti_inf/cw7/cw7.js
@@ -2,6 +2,9 @@
 const btnCreate = document.querySelector("#btnCreate");
 const input = document.querySelector("#inputText");
 const result = document.querySelector("#result");
+const btnReverse = document.querySelector("#btnReverse");
+const btnAdd = document.querySelector("#btnAdd");
+let isArrayCreated = false;
 btnCreate.onclick = function () {
     //pobranie wartości z pola tekstowego 
     // i konwersja na liczbę całkowitą lub NaN
@@ -16,4 +19,12 @@ btnCreate.onclick = function () {
     console.log(numbers);
     //wyświetlenie wyniku na stronie
     result.innerHTML = "Wylosowano liczby: " + numbers.join(" ");
+    isArrayCreated = true;
+    buttonsActive();
 };
+buttonsActive();
+function buttonsActive(){
+    //aktywacja przycisków dopiero po utworzeniu tablicy
+    btnReverse.disabled = !isArrayCreated;
+    btnAdd.disabled = !isArrayCreated;
+}
