1314c3342afb39d600cb2848ba6c0ed280b4198f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-05 10:35:27 +0100

    uu

---
+++ 2tip/2tip_desk/cw2_wpf/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cw2_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            var windowOther = new OtherWindow();
+            if (checkBox.IsChecked == true) {
+                windowOther.ShowDialog();
+            }
+            else {
+                windowOther.Show();
+            }
+                
+        }
+
+        private void ButtonClose(object sender, RoutedEventArgs e) {
+            //Close(); // closes only the MainWindow
+            Application.Current.Shutdown(); // closes the entire application
+        }
+    }
+}
---
+++ 2tip/2tip_desk/cw2_wpf/OtherWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cw2_wpf
+{
+    /// <summary>
+    /// Interaction logic for OtherWindow.xaml
+    /// </summary>
+    public partial class OtherWindow : Window
+    {
+        public OtherWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
