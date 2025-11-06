73c8e3247b3eb9bd391b414c48e247d7c78ea0bc
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-06 13:41:02 +0100

    kkk

--- 5tip/5tip_web/cw4-router/src/pages/Contacts.tsx
+++ 5tip/5tip_web/cw4-router/src/pages/Contacts.tsx
@@ -1,9 +1,60 @@
+import { use, useRef, useState, type FormEvent } from "react";
 
 type Props = {}
 
 const Contacts = (props: Props) => {
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLTextAreaElement>(null);
+  const educationRef = useRef<HTMLSelectElement>(null);
+  const [formData, setFormData] = useState<string | null>(null);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    console.log(e);
+    if(firstNameRef.current && descriptionRef.current && educationRef.current) {
+      const formData = {
+        firstname: firstNameRef.current.value,
+        description: descriptionRef.current.value,
+        education: educationRef.current.value
+      };
+      setFormData(JSON.stringify(formData));
+      console.log(formData);
+    }
+
+  }
+
   return (
-    <div>Contacts</div>
+    <>
+      <h1>Dane kontaktowe</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="firstname" >Imię:</label>
+          <input className="col-6" ref={firstNameRef} type="text" id="firstname" name="firstname" />
+        </div>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="description" >Opis:</label>
+          <textarea className="col-6" ref={descriptionRef} id="description" name="description" />
+        </div>
+        <div className="row m-2">
+          <label className="col-3 text-end" htmlFor="education" >Wykształcenie:</label>
+          <select className="col-6" ref={educationRef} id="education" name="education" >
+            <option value="podstawowe">Podstawowe</option>
+            <option value="średnie">Średnie</option>
+            <option value="wyższe">Wyższe</option>
+          </select>
+        </div>
+        <div className="row m-2">
+          <button
+            className="btn btn-primary col-6 offset-3"
+            type="submit">Wyślij</button>
+        </div>
+      </form>
+      <section>
+        {formData}
+      </section>
+    </>
+    // formularz kontaktowy za pomocą useRef i wyświetlenie danych w konsoli
+    //input (imię), textarea(opis siebie), select (wykształcenie)
   )
 }
 
--- 5tip/5tip_web/cw4-router/src/pages/Home.tsx
+++ 5tip/5tip_web/cw4-router/src/pages/Home.tsx
@@ -1,11 +1,35 @@
+import { useRef, useState } from "react";
 
 type Props = {}
 
 function Home({ }: Props) {
+    // useRef is used to create a mutable object
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [info, setInfo] = useState<string>("na początku było...");
+    console.log("renderowanie Home...");
+    
+    function handleInput(): void {
+        //pobranie wartości z inputa
+        if (inputRef.current) {
+            const inputValue = inputRef.current.value;
+            console.log(inputValue);
+            //zmiana stanu komponentu
+            setInfo(`Witaj ${inputValue}`);
+            console.log(info);            
+        }
+    }
+
     return (
         <>
-            <h1>Home</h1>
-            <p>Welcome to the home page!</p>
+            <h1>UseRef</h1>
+            <p>UseRef is a React Hook that allows you to
+                 create a mutable object which holds 
+                 a `.current` property. This object 
+                 persists for the full lifetime
+                  of the component.</p>
+             <input ref={inputRef} type="text" placeholder="podaj imię" />  
+            <button onClick={() => handleInput()}>Pobierz z inputa</button>
+             <span>{info}</span>   
         </>
     )
 }
