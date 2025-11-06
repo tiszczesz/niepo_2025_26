e60f85e65e5a7dd59b36122e818b92ee93b7cf22
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-06 12:26:02 +0100

    kk

---
+++ 3ti_inf/cw8/cw8.js
@@ -0,0 +1,32 @@
+const colors = [ //tworzenie tablicy z kolorami
+    "white",
+    "black",
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "purple",
+]
+//funkcja generująca select z opcjami kolorów
+function generateColorSelect(elem) {
+    let html  = '<select id="colorSelect">'; //tworzenie elementu select z id colorSelect
+    //iteracja po tablicy colors w celu wygenerowania wszystkich opcji
+    for(let i=0; i<colors.length;i++){
+        html += '<option value="' + colors[i] + '">' //<option value="red">
+               + colors[i] + '</option>'; //dodanie opcji do selecta
+    }
+    html += '</select>'; //zamknięcie elementu select
+    elem.innerHTML = html; //wstawienie wygenerowanego selecta do elementu przekazanego
+                        // jako argument
+}
+function changeBackgroundColor() {
+    const select = document.querySelector('#colorSelect'); //pobranie elementu select
+    const selectedColor = select.value; //pobranie wybranego koloru
+    document.body.style.backgroundColor = selectedColor; //zmiana koloru tła strony
+}
+//za funkcja
+const first = document.querySelector('#first'); //pobranie elementu o id first
+generateColorSelect(first); //wywołanie funkcji generującej select z kolorami 
+                            // i wstawienie go do elementu first
+const select = document.querySelector('#colorSelect');
+select.onchange = changeBackgroundColor; //ustawienie obsługi zdarzenia onchange
