9f96ebee1b504ac7f5fca1ee5a55d66963ab4b7f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-06 16:41:14 +0100

    kk

--- 4tip/4tip_type_script/cw5_web/src/index.ts
+++ 4tip/4tip_type_script/cw5_web/src/index.ts
@@ -1,3 +1,5 @@
-import { colors } from "./index_module.js";
+import { colors, generSelect } from "./index_module.js";
 console.log("Hello from TS");
 console.log(colors);
+document.querySelector<HTMLDivElement>("main")!
+    .appendChild(generSelect(colors));
--- 4tip/4tip_type_script/cw5_web/src/index_module.ts
+++ 4tip/4tip_type_script/cw5_web/src/index_module.ts
@@ -5,3 +5,15 @@ export const colors: string[] = [
     "blue",
     "yellow",
 ];
+export function generSelect(items: string[]): HTMLSelectElement {
+    const select = document.createElement("select");
+    for (const item of items) {
+        const option = document.createElement("option");
+        option.value = item;
+        option.innerText = item;
+        select.appendChild(option);
+        select.style.width = "250px";
+    }
+    select.id = "colorSelect";
+    return select;
+}
