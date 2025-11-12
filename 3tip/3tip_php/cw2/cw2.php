bc28f3a3f9527c1adafbd7d4cffa19c402376af5
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-12 12:17:58 +0100

    kk

---
+++ 3tip/3tip_php/cw2/cw2.php
@@ -0,0 +1,40 @@
+<?php
+$cookies_name = "user";
+$cookies_value = "re eer er123";
+setcookie(
+    $cookies_name,
+    $cookies_value,
+    time() + 86400
+); // 86400 = 1 day
+setcookie("test_cookie", "test");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ćwiczenie 2 - cookies</title>
+</head>
+
+<body>
+    <h1>Ćwiczenie 2 - cookies</h1>
+    <?php
+    echo "<pre>";
+    var_dump($_COOKIE);
+    echo "</pre>";
+    if (!isset($_COOKIE[$cookies_name])) {
+        echo "<p>Nazwa ciasteczka '" . $cookies_name . "' nie jest ustawiona!</p>";
+    } else {
+        echo "<p>Ciasteczko '" . $cookies_name . "' jest ustawione!<br>";
+        echo "Wartość: " . $_COOKIE[$cookies_name] . "</p>";
+    }
+    setcookie($cookies_name, "Nowa wartość", time() + 3600);
+    var_dump($_COOKIE);
+    // unset($_COOKIE[$cookies_name]);
+    // var_dump($_COOKIE);
+    //setcookie($cookies_name, "", time() - 3600); // usuwanie ciasteczka
+    ?>
+</body>
+
+</html>
