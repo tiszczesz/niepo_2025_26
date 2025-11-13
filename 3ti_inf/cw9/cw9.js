4f3ee53301c18c2482f13de965e83381d85e8de0
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-13 12:28:16 +0100

    kk

--- 3ti_inf/cw9/cw9.js
+++ 3ti_inf/cw9/cw9.js
@@ -1,3 +1,18 @@
-document.querySelector('#alert-button').onclick = function(){
+document.querySelector('#alert-button').onclick = function () {
     alert('Button clicked!');
+}
+document.querySelector('#confirm-button').onclick = function () {
+    const result = confirm('Będziesz się uczył programowania?');
+    console.log(result);
+    if (result) {
+        document.querySelector('#result-confirm').innerHTML
+            = 'Użytkownik będzie się uczył programowania';
+    } else {
+        document.querySelector('#result-confirm').innerHTML
+            = 'Użytkownik leje na programowanie i pożałuje!';
+    }
+}
+document.querySelector('#prompt-button').onclick = function () {
+    var name = prompt('Jak masz na imię?');
+    console.log(name);
 }
