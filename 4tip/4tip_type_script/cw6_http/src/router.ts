2d6e09befa645d637c63bf55478b919a00579482
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-14 12:25:59 +0100

    kk

--- 4tip/4tip_type_script/cw6_http/src/router.ts
+++ 4tip/4tip_type_script/cw6_http/src/router.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import {type Book, books} from './data/my-books.js';
 
 export const handleRoutes = (req: IncomingMessage, res: ServerResponse) => { 
     if(req.url === '/' && req.method === 'GET'){
@@ -9,6 +10,10 @@ export const handleRoutes = (req: IncomingMessage, res: ServerResponse) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         return res.end('<h1>About Page</h1><p>lorem ipsum</p>');
+    }else if(req.url === '/books' && req.method === 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify(books));
     }
     
     else{
