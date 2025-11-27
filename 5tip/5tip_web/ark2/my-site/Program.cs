bfe3194dc93da13b0e55f9f3d47d0e93e9f934ec
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-27 15:03:06 +0100

    kk

--- 5tip/5tip_web/ark2/my-site/Program.cs
+++ 5tip/5tip_web/ark2/my-site/Program.cs
@@ -1,12 +1,16 @@
+using my_site.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 //dodajemy obsługę kontrolerów
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 //doddajemy obsługę plików statycznych css js img...
 app.UseStaticFiles();
-
+//var connectionString = builder.Configuration.GetConnectionString("mysql");
+var repo = new CoursesRepo(builder.Configuration);
 //dodajemy obsługę routingu Home/Index
 app.MapDefaultControllerRoute();
 //app.MapGet("/", () => "Hello World!");
+app.MapGet("/api",()=>repo.GetAll());
 
 app.Run();
