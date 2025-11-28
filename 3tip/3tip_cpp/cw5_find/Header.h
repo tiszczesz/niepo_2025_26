aa2ba497eaf4288756096a3186fb123f6dd5d5c3
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-28 08:35:31 +0100

    pocczatek i instrukcja

---
+++ 3tip/3tip_cpp/cw5_find/Header.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+//napisz w pliku Functions.cpp funkcje:
+//1. Funkcja generujaca tablice n losowych liczb 
+//    calkowitych z przedzialu od min do max
+//2. Funkcja wyswietlajaca tablice
+//3. Funkcja sortujaca tablice metoda dowolna
+//4. Funkcja wyszukujaca w tablicy uporzadkowanej podanej liczby
---
+++ 3tip/3tip_cpp/cw5_find/Source.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <string>
+#include "Header.h"
+
+
+using namespace std;
+
+int main() {
+	
+	return 0;
+}
