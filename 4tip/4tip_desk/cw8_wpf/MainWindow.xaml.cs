09c1bbce18acce5cdc6b6e62bc231c4c6e4a48ab
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-11-28 10:59:58 +0100

    kk

---
+++ 4tip/4tip_desk/cw8_wpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cw8_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged {
+        private int _boundNumber;
+
+        public int BoundNumber {
+            get { return _boundNumber; }
+            set {
+                _boundNumber = value;
+                info.Content = _boundNumber;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string? name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;// Set the data context for data binding
+            BoundNumber = 67;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Reset_OnClick(object sender, RoutedEventArgs e) {
+            BoundNumber = 0;
+            MessageBox.Show(BoundNumber.ToString());
+        }
+
+        private void OpenWindow_OnClick(object sender, RoutedEventArgs e) {
+                throw new NotImplementedException();
+        }
+
+      
+    }
+}
