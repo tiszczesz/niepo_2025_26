b7146570e70053cb77aa4b37bca3555cc945f185
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-12-01 10:35:19 +0100

    kk

--- 3ti_inf/cw11/cw11.js
+++ 3ti_inf/cw11/cw11.js
@@ -19,5 +19,21 @@ const contacts = [
         name: "David Wilson",
         email: "david.wilson@example.com"
     },
-
 ];
+const left = document.querySelector('#left');
+const table = document.createElement('table');
+const thead = table.appendChild(document.createElement('thead'));
+const headerRow = document.createElement('tr');
+headerRow.innerHTML = `<th>Lp.</th>
+                      <th>Nazwa</th>
+                      <th>Email</th>`;
+thead.appendChild(headerRow);
+const tbody = table.appendChild(document.createElement('tbody'));
+for(let i=0; i<contacts.length;i++){
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${i+1}</td>
+                        <td>${contacts[i].name}</td>
+                        <td>${contacts[i].email}</td>`;
+    tbody.appendChild(row);
+}
+left.appendChild(table);
