a4fc616804b6c399a236ecf34b6bec784d4a547d
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-12-01 10:14:19 +0100

    kk

---
+++ 3ti_inf/cw11/cw11.js
@@ -0,0 +1,23 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Alice Johnson",
+    email: "alice.johnson@example.com"
+  },
+   {
+    id: 2,
+    name: "Bob Smith",
+    email: "bob.smith@example.com"
+  },
+   {
+    id: 3,
+    name: "Charlie Brown",
+    email: "charlie.brown@example.com"
+  },
+   {
+    id: 4,
+    name: "David Wilson",
+    email: "david.wilson@example.com"
+  },
+
+];
