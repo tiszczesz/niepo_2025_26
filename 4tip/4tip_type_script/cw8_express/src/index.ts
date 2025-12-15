fd46300ced98a6b0ae079c844b3a347a2ff9ee2b
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-12-15 08:58:18 +0100

    kk

--- 4tip/4tip_type_script/cw8_express/src/index.ts
+++ 4tip/4tip_type_script/cw8_express/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, TypeScript with Node.js and ES Modules!");
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
