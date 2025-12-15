f8b5c1f7bf212bf819577a55f1ec09c15b324d74
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-12-15 11:35:03 +0100

    kk

--- 2tip/2tip_desk/cw8_listBox2/Form1.Designer.cs
+++ 2tip/2tip_desk/cw8_listBox2/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             lbStudents = new ListBox();
             btnLoad = new Button();
+            lbListInfo = new Label();
+            btnDelete = new Button();
             SuspendLayout();
             // 
             // lbStudents
@@ -43,28 +45,56 @@
             // 
             // btnLoad
             // 
-            btnLoad.Location = new Point(336, 15);
+            btnLoad.Location = new Point(341, 45);
             btnLoad.Name = "btnLoad";
             btnLoad.Size = new Size(177, 58);
             btnLoad.TabIndex = 1;
             btnLoad.Text = "Wczytaj z pliku";
             btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // lbListInfo
+            // 
+            lbListInfo.AutoSize = true;
+            lbListInfo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            lbListInfo.ForeColor = Color.Maroon;
+            lbListInfo.Location = new Point(341, 9);
+            lbListInfo.Name = "lbListInfo";
+            lbListInfo.Size = new Size(170, 21);
+            lbListInfo.TabIndex = 2;
+            lbListInfo.Text = "Brak elementów listy";
+            // 
+            // btnDelete
+            // 
+            btnDelete.Enabled = false;
+            btnDelete.Location = new Point(341, 125);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(177, 58);
+            btnDelete.TabIndex = 3;
+            btnDelete.Text = "Usuń zaznaczonego studenta";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnDelete);
+            Controls.Add(lbListInfo);
             Controls.Add(btnLoad);
             Controls.Add(lbStudents);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListBox lbStudents;
         private Button btnLoad;
+        private Label lbListInfo;
+        private Button btnDelete;
     }
 }
--- 2tip/2tip_desk/cw8_listBox2/Form1.cs
+++ 2tip/2tip_desk/cw8_listBox2/Form1.cs
@@ -1,3 +1,4 @@
+﻿using System.ComponentModel;
 using cw8_listBox2.Models;
 
 namespace cw8_listBox2
@@ -9,6 +10,34 @@ namespace cw8_listBox2
         {
             InitializeComponent();
             _studentsRepo = new StudentsRepo("students.txt");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //wyczyść listbox
+            LoadToListBox();
+            btnDelete.Enabled = lbStudents.SelectedIndex != -1;
+        }
+
+        private void LoadToListBox()
+        {
+            lbStudents.DataSource = null;
+            //utwórz BindingList na podstawie listy studentów
+            //automatyczna aktualizacja listboxa przy zmianie listy
+            BindingList<Student> bindingList =
+                new BindingList<Student>(_studentsRepo.Students);
+            lbStudents.DataSource = bindingList;
+            lbListInfo.Text = "Ilość studentów: " + bindingList.Count;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // usuwanie zaznaczonego studenta z listy
+            var selectedStudent = lbStudents.SelectedItem as Student;
+            if (selectedStudent != null) {
+                _studentsRepo.Students.Remove(selectedStudent);
+                LoadToListBox();
+            }
         }
     }
 }
