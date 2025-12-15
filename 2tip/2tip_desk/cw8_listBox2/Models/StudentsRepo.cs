e4627cd109f6536646570405fd5953eb8e7a2587
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2025-12-15 11:08:32 +0100

    kk

--- 2tip/2tip_desk/cw8_listBox2/Models/StudentsRepo.cs
+++ 2tip/2tip_desk/cw8_listBox2/Models/StudentsRepo.cs
@@ -20,7 +20,21 @@ namespace cw8_listBox2.Models
             var result = new List<Student>();
             //wczytaj plik linia po linii do tablicy stringów
             var lines = File.ReadAllLines(_fileName);
-
+            //deserializacja danych z linii do obiektów Student
+            foreach (var line in lines) {
+                //podziel linię na części
+                var data = line.Split('|');
+                if (data.Length == 4) {
+                    result.Add(
+                            new Student() {
+                                Id = Convert.ToInt32(data[0]),
+                                FirstName = data[1].Trim(),
+                                LastName = data[2].Trim(),
+                                BirthDate = Convert.ToDateTime(data[3].Trim())
+                            }
+                        );
+                }
+            }
             return result;
         }
     }
