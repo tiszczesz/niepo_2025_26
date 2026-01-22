d9b4dc9c02316e70d83948064e1569f4b7f1e9bb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-01-22 11:53:11 +0100

    kk

--- 3ti_inf/cw13/cw13.js
+++ 3ti_inf/cw13/cw13.js
@@ -1,11 +1,12 @@
 const clients = ["Client A", "Client B", "Client C",
     "Client D", "Client E", "Client F", "Client G", "Client H", "Client I",
-    "Client J"];
+    "Client J", " Client K", "Client L", "Client M", "Client N"];
 function generAllClients(clients, elem) {
     for (let i = 0; i < clients.length; i++) {
         const div = document.createElement("div");
         div.className = "item";
         div.textContent = clients[i];
+        div.id = clients[i].toLowerCase().replace(" ", "-");
         elem.appendChild(div);
     }
 }
