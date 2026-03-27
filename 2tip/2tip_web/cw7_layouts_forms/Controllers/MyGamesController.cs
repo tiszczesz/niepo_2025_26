05b3a2082c73bd82f168af9f027afebfdd787590
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-03-27 15:45:17 +0100

    kk

---
+++ 2tip/2tip_web/cw7_layouts_forms/Controllers/MyGamesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw7_layouts_forms.Controllers
+{
+    public class MyGamesController : Controller
+    {
+        // GET: MyGamesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
