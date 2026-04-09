1880692d33847ea06eaae9b9276a2b6aff1976ac
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-04-09 12:23:15 +0200

    kk

--- 3ti_inf/cw14/cw14.js
+++ 3ti_inf/cw14/cw14.js
@@ -14,9 +14,17 @@ console.log(book2);
 document.querySelector("#result1")
     .textContent = `Tytuł: ${book1.title}, Cena: ${book1.price} zł,
            Data realizacji: ${book1.dateRealized}`;
-
+// Tworzenie obiektu za pomocą konstruktora 
+// czyli mamy wzorzec do tworzenia obiektów tego samego typu
 function Book(title, price, dateRealized) {
     this.title = title;
     this.price = price;
     this.dateRealized = dateRealized;
 }
+const book3 = new Book('1984', 9.99, '1949-06-08');
+const book4 = new Book('Ala ma kota', 119.99, '1999-06-08');
+console.log(book3);
+console.log(book4);
+document.querySelector("#result2")
+    .textContent = `Tytuł: ${book3.title}, Cena: ${book3.price} zł,
+           Data realizacji: ${book3.dateRealized}`;
