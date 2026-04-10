b877c532fcc7406bb82ece51a7a70c6d54a41684
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-04-10 12:04:00 +0200

    first

--- 4tip/4tip_mobile/cw1_android/app/src/main/java/com/example/cw1_android/MainActivity.kt
+++ 4tip/4tip_mobile/cw1_android/app/src/main/java/com/example/cw1_android/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.cw1_android
 
+import android.icu.util.GregorianCalendar
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //zainicjowanie kontrolek button i textview
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView2)
+        val button2 = findViewById<Button>(R.id.button2)
+        val textView3 = findViewById<TextView>(R.id.textView3)
+
+        //zmieniamy napis na textview przy starcie aplikacji
+        textView.text = "Hello World!"
+
+        //ustawiamy listener na button, który zmienia napis na textview po kliknięciu
+        button.setOnClickListener {
+            textView.text = "Button Clicked!"
+        }
+        button2.setOnClickListener {
+            val callendar = GregorianCalendar()
+            textView3.text = "Current date and time: ${callendar.time}"
+        }
+
     }
 }
