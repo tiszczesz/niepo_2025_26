f4a80454c311151ef055c192493d87c509eb00df
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-22 16:49:42 +0200

    kk

--- 2tip/2tip_web/web_solution/web.test/UnitTestUser.cs
+++ 2tip/2tip_web/web_solution/web.test/UnitTestUser.cs
@@ -6,6 +6,27 @@ namespace web.test;
 
 public class UnitTestUser
 {
+    public List<User> Users { get; set; }
+    [Fact]
+    public void If_Users_From_FakeRepo_Is_Not_Null()
+    {
+        // Arrange
+        var repo = new CompanyFakeRepo();
+        // Act
+        var users = repo.GetAllUsers();
+        // Assert
+        Assert.NotNull(users);
+    }
+    [Fact]
+    public void If_Users_From_FakeRepo_Has_2_Users()
+    {
+        // Arrange
+        var repo = new CompanyFakeRepo();
+        // Act
+        var users = repo.GetAllUsers();
+        // Assert
+        Assert.Equal(2, users.Count);
+    }
     [Fact]
     public void If_User_Is_Created()
     {
