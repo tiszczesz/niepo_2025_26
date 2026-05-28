de4d7f67ee4ab48711ebd5b734c5f4554ba8942e
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-28 09:22:02 +0200

    kk

--- 3tip/3tip_php/cw5_js/cw5_js.js
+++ 3tip/3tip_php/cw5_js/cw5_js.js
@@ -7,3 +7,13 @@ document.querySelector("#input").addEventListener("change", function () {
 const cities = ["Warszawa", "Kraków", "Łódź", "Wrocław",
     "Poznań", "Gdańsk", "Szczecin", "Bydgoszcz", "Lublin", "Białystok",
     "Katowice", "Gdynia", "Częstochowa", "Radom", "Sosnowiec"];
+function RenderCities() {
+    const list = document.querySelector("#cities");
+    list.innerHTML = "";
+    cities.forEach(city => {
+        const li = document.createElement("li");
+        li.textContent = city;
+        list.appendChild(li);
+    });
+}
+RenderCities();
