946629c5e5872369d4c84f2d7441112e863f38bf
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-05-29 16:50:15 +0200

    kk

--- 2tip/2tip_web/web_solution/web.app/Program.cs
+++ 2tip/2tip_web/web_solution/web.app/Program.cs
@@ -3,6 +3,7 @@ using web.app.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ICompanyRepo,CompanyFakeRepo>();
+//builder.Services.AddScoped<ICompanyRepo,CompanySqliteRepo>();
 var app = builder.Build();
 
 app.UseStaticFiles();
